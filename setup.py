d9a454a59acee6c6e4fd230c4d623b4170632e1a
Drew Arnett <a.arnett@ieee.org>
2017-09-30 06:17:24 -0700

    add setuptools setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pypicoboard',
+    version='0.0.0',
+    py_modules=['picoboard'],
+    author='Drew Arnett',
+    author_email='a.arnett@ieee.org',
+    description='Python library to use the Scratch PicoBoard',
+    url='https://github.com/drewarnett/pypicoboard'
+)
